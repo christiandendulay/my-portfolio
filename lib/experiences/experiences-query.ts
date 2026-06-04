06e23b12b37187cb568c249ebf765de6c05d93c5
christiandendulay <christian.den.dulay@gmail.com>
2026-06-04 08:45:11 +0800

    feat: experiences component

---
+++ lib/experiences/experiences-query.ts
@@ -0,0 +1,28 @@
+import { gql } from 'graphql-request';
+
+export const GET_EXPERIENCES = gql`
+  query GetExperiences {
+    experiencesCollection(where: { singletonId: "experiences" }, limit: 1) {
+      items {
+        sys {
+          id
+        }
+        singletonId
+        title
+        jobExperiencesCollection {
+          items {
+            sys {
+              id
+            }
+            company
+            startDate
+            isCurrent
+            locationType
+            endDate
+            role
+          }
+        }
+      }
+    }
+  }
+`;
