eb13e3a79423308e697e5a723ea2b78853122d6f
christiandendulay <christian.den.dulay@gmail.com>
2026-06-05 21:10:23 +0800

    feat: add divider component

---
+++ app/components/divider/Divider.tsx
@@ -0,0 +1,9 @@
+export function Divider() {
+  return (
+    <div className="mx-auto my-0 flex max-w-4xl items-center gap-4 px-5 py-8">
+      <div className="border-border flex-1 border-t" />
+      <div className="bg-teal h-1.5 w-1.5 rounded-full" />
+      <div className="border-border flex-1 border-t" />
+    </div>
+  );
+}
